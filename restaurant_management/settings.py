dbdd05c7d0199211ff5bc105250756cfc52c6905
NhaTruong <tt912002@gmail.com>
2024-10-22 11:50:23 +0700

    final folder tree of project

--- restaurant_management/settings.py
+++ restaurant_management/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'restaurant_management.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'restaurantdb',
+        'USER': 'root',
+        'PASSWORD': 'your_password',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
