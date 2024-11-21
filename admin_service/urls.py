201a088b20641ef9581369dd4a27f5ac2168a7f0
NhaTruong <tt912002@gmail.com>
2024-11-21 11:37:46 +0700

    new

--- admin_service/urls.py
+++
@@ -1,8 +0,0 @@
-# admin_service/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    # Định nghĩa các đường dẫn URL của bạn ở đây, ví dụ:
-    # path('some-url/', views.some_view, name='some_view'),
-]
