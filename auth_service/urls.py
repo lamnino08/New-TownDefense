b12f4f1a6301cbaa44ced76610934e43e22fba3d
lamnino08 <phanduclam02@gmail.com>
2024-11-21 14:10:16 +0700

    lam delete

--- auth_service/urls.py
+++
@@ -1,9 +0,0 @@
-# auth_service/urls.py
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('login/', views.login_view, name='login'),
-    path('api/login', views.login_api, name='login_api'),
-    path('logout/', views.logout_view, name='logout'),
-]
