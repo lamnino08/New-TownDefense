0c6c7af11d702f85d91c70998591861a3e7e8b88
DinhNam1714 <136407350+DinhNam1714@users.noreply.github.com>
2024-11-21 12:31:40 +0700

    nam delete

--- inventory_service/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Supplier
-
-class SupplierForm(forms.ModelForm):
-    class Meta:
-        model = Supplier
-        fields = ['supplier_code', 'name', 'address', 'phone', 'email']
